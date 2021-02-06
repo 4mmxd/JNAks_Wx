277d67c64b9bd43737b4bc9ddb7d44abe06acfaa
4mmxd <58057677+4mmxd@users.noreply.github.com>
2021-02-06 17:17:01 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,27 @@
+<script>
+	export default {
+		onLaunch: function() {
+			if (!wx.cloud) {
+				console.error('请使用 2.2.3 或以上的基础库以使用云能力');
+			} else {
+				wx.cloud.init({
+					env: 'gutint-k2wbl',
+					traceUser: true
+				});
+			}
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	page{
+		background: #EFF8FB 10%;
+	}
+</style>
